fb89729cd68a7dad57f3067b1acb3019b33e8ffa
xxsimoxx <simospa@gieffeedizioni.it>
2021-08-25 14:00:22 +0200

    version bump

--- codepotent-head-cleaner.php
+++ codepotent-head-cleaner.php
@@ -4,7 +4,7 @@
  * -----------------------------------------------------------------------------
  * Plugin Name: Head Cleaner
  * Description: Remove specific tags from the ClassicPress head section to reduce server requests and improve site performance.
- * Version: 1.2.1-rc1
+ * Version: 1.2.1
  * Author: Simone Fioravanti
  * Author URI: https://software.gieffeedizioni.it
  * Plugin URI: https://software.gieffeedizioni.it
